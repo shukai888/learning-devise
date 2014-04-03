adb52b0e22b501d39b2a4b0f264bd6ac46ee7fd0
shukai888 <shukai888@gmail.com>
2014-04-03 09:44:57 +0800

    basic devise

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LearningDevise::Application.config.secret_key_base = 'd320cb7c449a6d5b005b1dc5d466bf66fe8824605c086e608a3dd4526622c1b8aadf87200e0a7c803153a9e4d37d350107616f26564c10f6621a2282315efb70'
