ce17c49a6e122956d82fd47611fcf678766e9b90
shukai888 <shukai888@gmail.com>
2014-04-08 00:22:17 +0800

    bookmark scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 LearningDevise::Application.routes.draw do
+  resources :bookmarks
+
   get "home/index"
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
